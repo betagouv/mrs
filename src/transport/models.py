3effc473703f77432e1c1e81d2c3b45057fad6bf
jpic <jamespic@gmail.com>
2017-12-14 19:47:42 +0100

    File upload/delete on Transport Bill

--- src/transport/models.py
+++ src/transport/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.urls import reverse
 
-from mrsrequest.models import MRSAttachement
+from mrsrequest.models import MRSAttachement, MRSRequest
 
 
 class Transport(models.Model):
@@ -9,9 +10,18 @@ class Transport(models.Model):
         on_delete=models.CASCADE,
     )
 
-    date_depart = models.DateField(verbose_name='Date d\'aller')
-    date_return = models.DateField(verbose_name='Date de retour')
-    distance = models.IntegerField('Kilométrage total parcouru')
+    date_depart = models.DateField(
+        verbose_name='Date d\'aller',
+        null=True
+    )
+    date_return = models.DateField(
+        verbose_name='Date de retour',
+        null=True
+    )
+    distance = models.IntegerField(
+        'Kilométrage total parcouru',
+        null=True
+    )
 
     expense = models.DecimalField(
         decimal_places=2, max_digits=6,
@@ -23,6 +33,29 @@ class Transport(models.Model):
         ordering = ['mrsrequest']
 
 
+class BillManager(models.Manager):
+    def allowed_objects(self, request):
+        return Bill.objects.filter(
+            transport__mrsrequest__in=MRSRequest.objects.allowed_objects(
+                request))
+
+    def record_upload(self, mrsrequest, upload):
+        '''
+        Create a Bill object from the upload on the request's transport.
+
+        When we want to support multiple forms in the future, we'll have a form
+        number in the field_name attribute of the upload.
+        '''
+        return Bill.objects.update_or_create(
+            transport=Transport.objects.get_or_create(
+                mrsrequest=mrsrequest)[0],
+            defaults=dict(
+                filename=str(upload),
+                binary=MRSAttachement.get_upload_body(upload),
+            )
+        )[0]
+
+
 class Bill(MRSAttachement):
     transport = models.ForeignKey(
         'Transport',
@@ -31,6 +64,11 @@ class Bill(MRSAttachement):
     binary = models.BinaryField(
         verbose_name='Justificatif de Transport')
 
+    objects = BillManager()
+
     class Meta:
         ordering = ['transport', 'id']
         verbose_name = 'Justificatif'
+
+    def get_delete_url(self):
+        return reverse('bill_delete', args=[self.pk])
