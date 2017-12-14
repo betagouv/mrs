b1c130d75d24823450eb349abb1051856411ba7b
jpic <jamespic@gmail.com>
2017-12-14 13:37:01 +0100

    PMT Upload progress

---
+++ conftest.py
@@ -0,0 +1,4 @@
+import pytest
+
+
+id = pytest.fixture(lambda: '2b88b740-3920-44e9-b086-c851f58e7ea7')
--- src/pmt/tests/test_pmt_views.py
+++ src/pmt/tests/test_pmt_views.py
@@ -1,46 +1,26 @@
 import io
+import uuid
 
-from dbdiff.fixture import Fixture
 from freezegun import freeze_time
 import pytest
 
-from django.urls import reverse
-
 from mrsrequest.models import MRSRequest
+from mrsrequest.tests.utils import upload_request
 from pmt.models import PMT
 from pmt.views import PMTUploadView
 
 
-id = pytest.fixture(lambda: '2b88b740-3920-44e9-b086-c851f58e7ea7')
-
-
-def upload_request(rf, id, file):
-    request = rf.post(
-        reverse('pmt_upload', args=[id]),
-        dict(file=file)
-    )
-    # Middleware is not called, deal with session manually
-    request.session = dict()
-    return request
-
-
 @pytest.mark.django_db
 @freeze_time('2000-01-02 03:04:05')
-def test_pmt_upload_on_allowed_id(rf, id):
+def test_pmtuploadview_create_object(rf, id):
+    view = PMTUploadView()
+    view.mrsrequest = MRSRequest.objects.create(id=uuid.uuid4())
     with io.BytesIO(b'lol') as f:
-        request = upload_request(rf, id, f)
-        MRSRequest(id=id).allow(request)
-        PMTUploadView().dispatch(request, mrsrequest_uuid=id)
-
-    Fixture(
-        './src/pmt/tests/test_pmt_views.test_pmt_upload_on_allowed_id.json',
-        models=[MRSRequest, PMT]
-    ).assertNoDiff()
-
-
-@pytest.mark.django_db
-def test_pmt_upload_on_not_allowed_id(rf, id):
-    with io.BytesIO(b'lol') as f:
-        request = upload_request(rf, id, f)
-        response = PMTUploadView().dispatch(request, mrsrequest_uuid=id)
-    assert response.status_code == 400
+        f.name = 'lol.jpg'
+        view.upload = upload_request(rf, id, f).FILES['file']
+        result = view.create_object()
+    assert isinstance(result, PMT)
+    assert result.pk
+    assert result.mrsrequest.id == view.mrsrequest.id
+    assert result.filename == 'lol.jpg'
+    assert result.binary == b'lol'
