519a78e262eb9c3ed4351bb14ffb2220f1e8c3d2
jpic <jamespic@gmail.com>
2017-12-14 19:12:28 +0100

    Add test_pmtmanager_allowed_objects

--- src/pmt/tests/test_pmt_models.py
+++ src/pmt/tests/test_pmt_models.py
@@ -1,25 +1,24 @@
 import io
 import pytest
-import uuid
 
 from freezegun import freeze_time
 
 from django.urls import reverse
 
-from mrsrequest.models import MRSRequest
-from mrsrequest.tests.utils import upload_request
+from mrsrequest.tests.utils import sessions, upload_request
 from pmt.models import PMT
+
+from pmt.tests.test_pmt_views import mrsrequest, pmt  # noqa
 
 
 def test_pmt_get_delete_url():
     assert PMT(id=3).get_delete_url() == reverse('pmt_delete', args=[3])
 
 
-@pytest.mark.django_db
+@pytest.mark.django_db  # noqa
 @freeze_time('2000-01-02 03:04:05')
-def test_pmtmanager_record_upload(rf):
+def test_pmtmanager_record_upload(rf, mrsrequest):
     '''Test record_upload()'''
-    mrsrequest = MRSRequest.objects.create(id=uuid.uuid4())
     with io.BytesIO(b'lol') as f:
         f.name = 'lol.jpg'
         upload = upload_request(rf, id, f).FILES['file']
@@ -30,3 +29,19 @@ def test_pmtmanager_record_upload(rf):
     assert result.mrsrequest.id == mrsrequest.id
     assert result.filename == 'lol.jpg'
     assert result.binary == b'lol'
+
+
+@pytest.mark.django_db  # noqa
+@pytest.mark.parametrize("session", sessions)
+def test_pmtmanager_allowed_objects(rf, pmt, session):
+    request = rf.get('/')
+    request.session = session
+
+    # Test Deny
+    assert PMT.objects.allowed_objects(request).count() == 0
+
+    # Allow
+    pmt.mrsrequest.allow(request)
+
+    # Test allow
+    assert list(PMT.objects.allowed_objects(request)) == [pmt]
