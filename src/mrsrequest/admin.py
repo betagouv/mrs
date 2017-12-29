f1fd278b0ddd0ea1e5203d14b32cc6ed1172f880
jpic <jamespic@gmail.com>
2017-12-29 04:59:46 +0100

    Ajout MRSRequestAdminForm

--- src/mrsrequest/admin.py
+++ src/mrsrequest/admin.py
@@ -22,18 +22,37 @@ class MRSRequestAdmin(admin.ModelAdmin):
     )
 
     list_display = (
-        'creation_datetime',
-        'insured',
+        'form_id',
+        'insured_first_name',
+        'insured_last_name',
+        'insured_nir',
         'status',
     )
     search_fields = (
         'insured__first_name',
         'insured__last_name',
         'insured__email',
+        'insured__nir',
+        'form_id',
     )
     list_filter = (
         'status',
     )
+
+    def insured_first_name(self, obj):
+        if obj.insured:
+            return obj.insured.first_name
+    insured_first_name.admin_order_field = 'insured__first_name'
+
+    def insured_last_name(self, obj):
+        if obj.insured:
+            return obj.insured.last_name
+    insured_last_name.admin_order_field = 'insured__last_name'
+
+    def insured_nir(self, obj):
+        if obj.insured:
+            return obj.insured.nir
+    insured_nir.admin_order_field = 'insured__nir'
 
     @csrf_protect_m
     @transaction.atomic
