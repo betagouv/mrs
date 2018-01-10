90e9ffb67f964f7e6b09a01f2a452c4e485c0bc9
jpic <jamespic@gmail.com>
2018-01-10 23:18:59 +0100

    Fix #71: Piwik code for production

--- src/mrs/settings.py
+++ src/mrs/settings.py
@@ -83,6 +83,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "contact.context_processors.contact_form",
+                "mrs.context_processors.settings",
             ],
         },
     },
