d5e5ec49fa758044494bc461db48e9e4ecce719e
Thomas Binetruy <tbinetruy@gmail.com>
2018-01-29 20:15:17 -0500

    created class to update UI on form submission, success, and error

---
+++ src/mrs/static/js/submit-ui.js
@@ -0,0 +1,123 @@
+/**
+ * Ajax loading UI component. Updates the DOM to show loader
+ * while request is processing. Shows error or success after
+ * request is finished
+ * @param {dom el} mountPoint where to insert form submission state UI
+ */
+export default class {
+    constructor(mountPoint) {
+        this.mountPoint = mountPoint
+        this.document = this.mountPoint.ownerDocument
+
+        this.overlay = this.createOverlay()
+    }
+
+    /**
+     * Creates overlay for form submission info
+     */
+    createOverlay(){
+        // insert child node of overlay
+        const loadingOverlay = this.document.createElement('DIV')
+        loadingOverlay.style.cssText = `
+            position: fixed;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            opacity: 0;
+            background-color: rgba(0, 0, 0, 0.2);
+            zIndex: 2;
+            transition: opacity 0.2s linear;
+        `
+        this.mountPoint.appendChild(loadingOverlay)
+
+        return loadingOverlay
+    }
+
+    /**
+     * Shows form processing overlay
+     */
+    showOverlay() {
+        this.overlay.style.opacity = 1
+    }
+
+    /**
+     * Hides form processing overlay
+     */
+    hideOverlay() {
+        this.overlay.style.opacity = 0
+    }
+
+    /**
+     * Removes overlay content
+     */
+    removeOverlayContent() {
+        if(this.overlay.childNodes.length) {
+            this.overlay.removeChild(this.overlay.firstChild)
+        }
+    }
+
+    /**
+     * Shows UI submission loader overlay
+     */
+    showSubmitLoading() {
+        this.removeOverlayContent()
+
+        const loader = this.document.createElement('DIV')
+        const text = this.document.createTextNode('Loading...')
+        loader.appendChild(text)
+
+        loader.style.cssText = `
+            height: 2rem;
+            width: 2rem;
+            background-color: red;
+        `
+
+        this.overlay.appendChild(loader)
+    }
+
+    /**
+     * Shows UI form submission error message
+     * @param {text} errorMsg error message to show
+     */
+    showSubmitError(errorMsg) {
+        this.removeOverlayContent()
+
+        const errorWrapper = this.document.createElement('DIV')
+        const errorText = this.document.createTextNode(errorMsg)
+        errorWrapper.appendChild(errorText)
+
+        errorWrapper.style.cssText = `
+            height: 2rem;
+            width: 2rem;
+            background-color: red;
+        `
+
+        this.overlay.appendChild(errorWrapper)
+    }
+
+    /**
+     * Shows UI form submission success message
+     * @param {text} successMsg success message to show
+     */
+    showSubmitSuccess(successMsg) {
+        this.removeOverlayContent()
+
+        const successWrapper = this.document.createElement('DIV')
+        const successText = this.document.createTextNode(successMsg)
+        successWrapper.appendChild(successText)
+
+        successWrapper.style.cssText = `
+            height: 2rem;
+            width: 2rem;
+            background-color: red;
+        `
+
+        this.overlay.appendChild(successWrapper)
+    }
+}
