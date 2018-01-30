0abe2a0b9a32f674ee9fc64a267339b467c97cca
Thomas Binetruy <tbinetruy@gmail.com>
2018-01-29 20:15:17 -0500

    created class to update UI on form submission, success, and error

---
+++ src/mrs/static/js/submit-ui.test.js
@@ -0,0 +1,87 @@
+/* global describe, jest, test, expect */
+
+import jsdom from 'jsdom'
+const { JSDOM } = jsdom
+import SubmitUi from './submit-ui.js'
+
+const domFixture = () => new JSDOM()
+const submitUiFactory = () => new SubmitUi(
+    domFixture().window.document.querySelector('body'))
+
+describe('submit-ui dom tests', () => {
+    test('showSubmitLoading', () => {
+        const submitUi = submitUiFactory()
+        submitUi.createOverlay()
+        const overlay = submitUi.mountPoint.querySelectorAll('div')
+
+        expect(overlay.length).toEqual(2)
+        expect(overlay[0].style.position).toEqual('fixed')
+    })
+
+    test('showOverlay()', () => {
+        const submitUi = submitUiFactory()
+        submitUi.showOverlay()
+
+        const overlay = submitUi.mountPoint.querySelectorAll('div')[0]
+        expect(overlay.style.opacity).toBe('1')
+    })
+
+    test('hideOverlay()', () => {
+        const submitUi = submitUiFactory()
+        submitUi.hideOverlay()
+
+        const overlay = submitUi.mountPoint.querySelectorAll('div')[0]
+        expect(overlay.style.opacity).toBe('0')
+    })
+
+    test('showSubmitLoading()', () => {
+        const submitUi = submitUiFactory()
+        submitUi.removeOverlayContent = jest.fn()
+
+        submitUi.showSubmitLoading()
+
+        expect(submitUi.removeOverlayContent.mock.calls).toEqual([[]])
+        expect(submitUi.overlay.querySelectorAll('div').length).toBe(1)
+    })
+
+    test('showSubmitError()', () => {
+        const submitUi = submitUiFactory()
+        submitUi.removeOverlayContent = jest.fn()
+
+        const errorMsg = 'error'
+        submitUi.showSubmitError(errorMsg)
+
+        expect(submitUi.removeOverlayContent.mock.calls).toEqual([[]])
+        expect(submitUi.overlay.querySelectorAll('div').length).toBe(1)
+        expect(submitUi.overlay.querySelectorAll('div')[0].innerHTML)
+            .toEqual(errorMsg)
+    })
+
+    test('showSubmitSuccess()', () => {
+        const submitUi = submitUiFactory()
+        submitUi.removeOverlayContent = jest.fn()
+
+        const successMsg = 'Success !'
+        submitUi.showSubmitSuccess(successMsg)
+
+        expect(submitUi.removeOverlayContent.mock.calls).toEqual([[]])
+        expect(submitUi.overlay.querySelectorAll('div').length).toBe(1)
+        expect(submitUi.overlay.querySelectorAll('div')[0].innerHTML)
+            .toEqual(successMsg)
+    })
+
+    test('removeOverlayContent', () => {
+        const submitUi = submitUiFactory()
+
+        // test when overlay is already empty
+        submitUi.removeOverlayContent()
+
+        expect(submitUi.overlay.querySelectorAll('div').length).toBe(0)
+
+        // test when overlay has content
+        submitUi.showSubmitLoading()
+        submitUi.removeOverlayContent()
+
+        expect(submitUi.overlay.querySelectorAll('div').length).toBe(0)
+    })
+})
