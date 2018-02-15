79e915b512e5b4ccc577a5764d5559f5b1d484fa
jpic <jamespic@gmail.com>
2018-02-15 17:33:05 +0100

    Test institution model methods

---
+++ src/institution/tests/test_institution_models.py
@@ -0,0 +1,17 @@
+import pytest
+
+from django.core.exceptions import ValidationError
+
+from institution.models import Institution
+
+
+def test_institution_str():
+    assert str(Institution(finess=310000000)) == '310000000'
+
+
+@pytest.mark.django_db
+def test_institution_finess():
+    Institution(finess=310000000).full_clean()
+
+    with pytest.raises(ValidationError):
+        Institution(finess=30999999).full_clean()
