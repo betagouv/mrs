4cf4b3ddd34b82e83c68025e8a5ed42adab98381
Thomas Binetruy <tbinetruy@gmail.com>
2018-03-12 01:57:28 -0400

    finished header fat mobile

--- src/mrs/static/js/components/Header.js
+++ src/mrs/static/js/components/Header.js
@@ -1,12 +1,110 @@
-import { h, render } from 'preact'
+import { h, render, Component } from 'preact'
+import * as UI from './UI'
 
-const Header = props => {
+const Link = props => {
     return (
-        <div className="Header--wrapper">
-          <span>Hello, world!</span>
-          <button onClick={ e => alert("hi!") }>Click Me</button>
+        <a
+          href={ props.url }
+          className="link"
+          key={ props.key }>
+          { props.alias }
+        </a>
+    )
+}
+const MenuLinks = props => {
+    const links = props.links
+          .map((e, i) => <Link key={ i } url={ e.url } alias={ e.alias } />)
+
+    const isOpened = props.isOpened ? "" : " hidden"
+    return (
+        <div className={ "menu-links--wrapper" + isOpened }>
+          { links }
         </div>
     )
 }
 
-export default Header
+MenuLinks.defaultProps = {
+    links: [],
+}
+
+const HeaderFat = props => {
+    const links = [
+        {
+            url: '/',
+            alias: 'fonctionnement',
+        },
+        {
+            url: '/foo',
+            alias: 'formulaire',
+        },
+        {
+            url: '/bar',
+            alias: 'faq',
+        },
+        {
+            url: '/bar',
+            alias: 'contact',
+        },
+    ]
+
+    return (
+        <UI.Row>
+          <div className="mrs-logo--wrapper">
+            <img
+                src="/static/img/logos/mrs.png"
+                alt="mrs"
+                className="logo-mrs" />
+          </div>
+          <MenuLinks
+            links={ links }
+            isOpened={ props.isMenuOpened } />
+          <div
+            onClick={ props.toggleMenu }
+            className="burger--wrapper">
+            <img
+                src="/static/img/icones/burger.png"
+                alt="menu"
+                className={ "burger" + (!props.isMenuOpened ? "" : " hidden") } />
+            <img
+              src="/static/img/icones/croix.png"
+              alt="femer"
+              className={ "burger close " + (props.isMenuOpened ? "" : " hidden") } />
+          </div>
+        </UI.Row>
+    )
+}
+
+const Header = props => {
+    return (
+        <div className="Header--wrapper">
+          <HeaderFat { ...props } />
+        </div>
+    )
+}
+
+class HeaderSmart extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isOpened: false,
+        }
+
+        this.toggleIsOpened = this.toggleIsOpened.bind(this)
+    }
+
+    toggleIsOpened() {
+        this.setState({ isOpened: !this.state.isOpened })
+    }
+
+    render() {
+        return <Header
+            toggleMenu={ this.toggleIsOpened }
+            isMenuOpened={ this.state.isOpened } />
+    }
+
+}
+
+export default HeaderSmart
+export {
+    HeaderFat,
+}
