ee685fdc98e3443b70ce38cacba090e57d59f6ce
Thomas Binetruy <tbinetruy@gmail.com>
2018-03-12 19:42:04 -0400

    exported statistics.js

--- src/mrs/static/js/statistics.js
+++ src/mrs/static/js/statistics.js
@@ -2,4 +2,8 @@
 import 'jpic-iframe-autoheight'
 import '../sass/statistics.sass'
 
-window.autoheight('autoheight')
+const init = () => {
+    window.autoheight('autoheight')
+}
+
+export default init
