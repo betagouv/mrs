247b48d613d3537c465501009e91bedb253b9312
jpic <jamespic@gmail.com>
2018-03-14 07:39:29 +0100

    wip

--- src/mrs/static/js/smoke.test.js
+++ src/mrs/static/js/smoke.test.js
@@ -3,8 +3,10 @@
 
 import jsdom from 'jsdom'
 
-describe('smoke test', () => {
-  test('should open a page', () => {
+var display = dom, selector => dom.window.document.querySelector(selector).style.display
+
+describe('index', () => {
+  test('should display contact and mrsrequest forms', () => {
     const { JSDOM } = jsdom
     return JSDOM.fromURL(
       'http://localhost:8000',
@@ -17,9 +19,11 @@ describe('smoke test', () => {
         dom.window.document.addEventListener('DOMContentLoaded', () => resolve(dom))
       })
     }).then(dom => {
-      let display = dom.window.document.querySelector('#mrsrequest-wizard').style.display
+      let mrsrequest = display(dom, '#mrsrequest-wizard')
+      let contact = display(dom, '#contact')
       dom.window.close()
-      expect(display).toEqual('')
+      expect(mrsrequest).toEqual('')
+      expect(id).toEqual('')
     })
   })
 })
