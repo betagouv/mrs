158f5a6e2946f9be9819b3c6c6434bfb861911b6
jpic <jamespic@gmail.com>
2018-03-14 18:59:31 +0100

    Fix #177: Changer la colonne Caisse.numero en varchar avec preformatage

--- src/caisse/models.py
+++ src/caisse/models.py
@@ -1,4 +1,15 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+
+
+def validate_caisse_number(value):
+    try:
+        value = int(value)
+    except ValueError:
+        raise ValidationError('Doit etre numerique')
+
+    if not 0 < value < 999:
+        raise ValidationError('Doit etre entre 0 et 999')
 
 
 class Caisse(models.Model):
@@ -7,8 +18,10 @@ class Caisse(models.Model):
         verbose_name='nom',
         max_length=50,
     )
-    number = models.PositiveIntegerField(
+    number = models.CharField(
+        max_length=3,
         verbose_name='numéro',
+        validators=[validate_caisse_number],
     )
     liquidation_email = models.EmailField(
         verbose_name='email vers les liquidateureuses',
@@ -25,3 +38,8 @@ class Caisse(models.Model):
 
     def __str__(self):
         return self.name
+
+
+def caisse_number_format(sender, instance, **kwargs):
+    instance.number = '{:03d}'.format(int(instance.number))
+models.signals.pre_save.connect(caisse_number_format, sender=Caisse)
