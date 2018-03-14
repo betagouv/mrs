86f0c4c847a79d6f8e7d3aa86bc1e2f552086077
jpic <jamespic@gmail.com>
2018-03-14 15:53:50 +0100

    institution

--- src/mrs/static/js/smoke.test.js
+++ src/mrs/static/js/smoke.test.js
@@ -2,23 +2,27 @@
 
 
 import jsdom from 'jsdom'
+const { JSDOM } = jsdom
 
 var display = (dom, selector) => dom.window.document.querySelector(selector).style.display
 
-describe('index', () => {
-  test('should display contact and mrsrequest forms', () => {
-    const { JSDOM } = jsdom
-    return JSDOM.fromURL(
-      'http://localhost:8000',
-      {
-        resources: 'usable',
-        runScripts:'dangerously'
-      }
-    ).then(dom => {
-      return new Promise(resolve => {
-        dom.window.document.addEventListener('DOMContentLoaded', () => resolve(dom))
-      })
-    }).then(dom => {
+function fetch(url) {
+  return JSDOM.fromURL(
+    'http://localhost:8000',
+    {
+      resources: 'usable',
+      runScripts:'dangerously'
+    }
+  ).then(dom => {
+    return new Promise(resolve => {
+      dom.window.document.addEventListener('DOMContentLoaded', () => resolve(dom))
+    })
+  })
+}
+
+describe('smoke tests', () => {
+  test('index should display contact and mrsrequest forms', () => {
+    return fetch('/').then(dom => {
       let mrsrequest = display(dom, '#mrsrequest-wizard')
       let contact = display(dom, '#contact')
       dom.window.close()
@@ -26,5 +30,12 @@ describe('index', () => {
       expect(contact).toEqual('')
     })
   })
-})
 
+  test('institution iframe', () => {
+    return fetch('/institution/310000000/iframe/').then(dom => {
+      let mrsrequest = display(dom, '#mrsrequest-wizard')
+      dom.window.close()
+      expect(mrsrequest).toEqual('')
+    })
+  })
+})
