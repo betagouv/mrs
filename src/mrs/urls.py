1996bbc0cd89ef79f4144f93411855b8876d14dc
jpic <jamespic@gmail.com>
2018-03-14 18:15:28 +0100

    contactrefactorwhatever

--- src/mrs/urls.py
+++ src/mrs/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+from contact.views import ContactView
 from mrsrequest.views import MRSRequestCreateView
 
 admin.site.site_header = 'MRS Admin'
@@ -12,6 +13,7 @@ urlpatterns = [
         template_name='index.html'), name='index'),
     path('', include('mrspage.urls', namespace='mrspage')),
     path('demande', MRSRequestCreateView.as_view(), name='demande'),
+    path('contact', ContactView.as_view(), name='contact'),
     path('contact/', include('contact.urls', namespace='contact')),
     path('mrsrequest/', include('mrsrequest.urls', namespace='mrsrequest')),
     path('institution/', include('institution.urls', namespace='institution')),
