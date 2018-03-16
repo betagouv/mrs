2d9af8095401bb850bdc8357034bf5c82b1543fa
Thomas Binetruy <tbinetruy@gmail.com>
2018-03-16 18:16:45 -0400

    updated caroussel mobile

--- src/mrs/static/js/components/Caroussel.js
+++ src/mrs/static/js/components/Caroussel.js
@@ -30,27 +30,37 @@ const CarouselUI = props => {
     )
 }
 
+const CitationSlide = props => {
+    return (
+        <CarouselSlide>
+            <div className="citation">{ props.citation }</div>
+            <div className="author">{ props.author }</div>
+        </CarouselSlide>
+    )
+}
+
+CitationSlide.defaultProps = {
+    citation: 'citation',
+    author: 'author',
+}
+
 const CarouselDumb = props => {
+    const slides = props.slides.map((e, i) =>
+                                    <CitationSlide
+                                    author={ e.author }
+                                    citation={ e.citation } />)
+
     return (
         <CarouselUI { ...props }>
           <div className="carousel carousel-slider">
-            <CarouselSlide>
-              <div>TEST1</div>
-            </CarouselSlide>
-
-            <CarouselSlide>
-              <div>TEST2</div>
-            </CarouselSlide>
-
-            <CarouselSlide>
-              <div>TEST3</div>
-            </CarouselSlide>
+            { slides }
           </div>
         </CarouselUI>
     )
 }
 
 CarouselDumb.defaultProps = {
+    slides: [],
     pervSlide: () => {},
     nextSlide: () => {},
 }
@@ -68,6 +78,7 @@ class Carousel extends Component {
 
         this.nextSlide = this.nextSlide.bind(this)
         this.prevSlide = this.prevSlide.bind(this)
+        this.startTimer = this.startTimer.bind(this)
     }
 
     componentDidMount() {
@@ -80,10 +91,20 @@ class Carousel extends Component {
 
         const instance = M.Carousel.init(elem, options)
         this.setState({ instance })
+
+        this.startTimer(instance)
+    }
+
+    startTimer(instance) {
+        this.timer = setInterval(() => instance.next(), 5000)
     }
 
     componentWillUnmount() {
+        // destroy swiper instance
         this.state.instance.destroy()
+
+        // stop timer
+        clearInterval(this.timer)
     }
 
     nextSlide() {
@@ -98,11 +119,29 @@ class Carousel extends Component {
         return (
             <div class="carousel--wrapper">
               <CarouselDumb
+                slides={ this.props.slides }
                 prevSlide={ this.prevSlide() }
                 nextSlide={ this.nextSlide() } />
             </div>
         )
     }
+}
+
+Carousel.defaultProps = {
+    slides: [
+        {
+            citation: 'Bonjour, j’ai bien reçu le remboursement pour mes frais de transport en voiture particulière, paiement très rapide et simplifié grâce à l’envoi par internet! Merci !',
+            author: 'Amelie F.',
+        },
+        {
+            citation: '...sympa. Je reste à votre disposition et très belle initiative que la vôtre',
+            author: 'Jean-Marie P.',
+        },
+        {
+            citation: 'J’ai eu le plaisir de faire partie des premières patientes à être remboursées grâce à ce système. Ce fut très rapide donc fort appréciable ! En espérant que bientôt tous les moyens transports soient également remboursés grâce à ce système. Merci pour les patients de mettre en place ce genre d’initiative',
+            author: 'Natacha B. ',
+        },
+    ]
 }
 
 export default Carousel
