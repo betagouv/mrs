208550c19e8d8b7bdf26dad102a5ad66d20b2374
jpic <jamespic@gmail.com>
2018-03-23 16:57:40 +0100

    Bichonage MRSEmail

--- src/mrsemail/models.py
+++ src/mrsemail/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 
 class EmailTemplate(models.Model):
-    name = models.CharField(max_length=50)
-    subject = models.CharField(max_length=255)
-    body = models.TextField()
+    name = models.CharField(max_length=50, verbose_name='Nom')
+    subject = models.CharField(max_length=255, verbose_name='Modèle de sujet')
+    body = models.TextField(verbose_name='Modèle de contenu')
 
     class Meta:
         ordering = ('name',)
+        verbose_name = 'Modèle d\'email'
+        verbose_name_plural = 'Modèles d\'emails'
 
     def __str__(self):
         return self.name
