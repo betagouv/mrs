eb2f05924e6083c063ee40d4e254a282a0255ad9
jpic <jamespic@gmail.com>
2018-03-27 12:46:28 +0200

    Fix #188: rm mrspage module

--- src/mrs/urls.py
+++ src/mrs/urls.py
@@ -13,11 +13,11 @@ admin.site.site_title = 'MRS Admin'
 urlpatterns = [
     path('', MRSRequestCreateView.as_view(
         template_name='index.html'), name='index'),
-    path('', include('mrspage.urls', namespace='mrspage')),
     path('demande', MRSRequestCreateView.as_view(), name='demande'),
     path('contact', ContactView.as_view(), name='contact'),
     path('mentions-legales', views.LegalView.as_view(), name='legal'),
     path('faq', views.FaqView.as_view(), name='faq'),
+    path('stats/', views.StatisticsView.as_view(), name='statistics'),
     path('contact/', include('contact.urls', namespace='contact')),
     path('mrsrequest/', include('mrsrequest.urls', namespace='mrsrequest')),
     path('institution/', include('institution.urls', namespace='institution')),
--- src/mrspage/views.py
+++
@@ -1,5 +0,0 @@
-from django.views import generic
-
-
-class StatisticsView(generic.TemplateView):
-    template_name = 'mrspage/statistics.html'
