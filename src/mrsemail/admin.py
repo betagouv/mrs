c0eb793a22dc5372164f18be0490bfc5a3f9921d
jpic <jamespic@gmail.com>
2018-04-05 14:40:35 +0200

    Fix #240: Save reject email template

--- src/mrsemail/admin.py
+++ src/mrsemail/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
+from django.db.models import Count
 
 from .models import EmailTemplate
 
 
-admin.site.register(EmailTemplate)
+class EmailTemplateAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'subject',
+        'requests',
+        'active',
+    ]
+    list_display_links = ['name']
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            requests=Count('mrsrequest'))
+
+    def requests(self, obj):
+        return obj.requests
+    requests.admin_order_field = 'requests'
+    requests.short_description = 'Demandes'
+
+    def has_delete_permission(self, *args, **kwargs):
+        return False
+
+admin.site.register(EmailTemplate, EmailTemplateAdmin)
