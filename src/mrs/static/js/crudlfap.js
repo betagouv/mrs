3acfd639c214a1cb23d376b6c95bf1bb379dfabe
angelo510 <devwiz.sh@gmail.com>
2018-04-17 09:45:26 -0700

    fix prototype is supported by IE

--- src/mrs/static/js/crudlfap.js
+++ src/mrs/static/js/crudlfap.js
@@ -11,6 +11,23 @@ import 'crudlfap/js/style.sass'
     var Turbolinks = require('turbolinks')
     Turbolinks.start()
   }
+  
+  //support to IE
+  if (!Element.prototype.matches)
+  Element.prototype.matches = Element.prototype.msMatchesSelector ||
+    Element.prototype.webkitMatchesSelector
+
+  if (!Element.prototype.closest) {
+    Element.prototype.closest = function(s) {
+        var el = this;
+        if (!document.documentElement.contains(el)) return null;
+        do {
+            if (el.matches(s)) return el;
+            el = el.parentElement || el.parentNode;
+        } while (el != null && el.nodeType === 1);
+        return el;
+    }
+  }
 }).bind(window)()
 
 const application = Application.start()
