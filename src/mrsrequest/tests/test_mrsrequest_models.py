4cf5dfb88bf7198ec1c7fbe2b2c3c34d0430be69
jpic <jamespic@gmail.com>
2018-04-20 18:24:22 +0200

    Fix #246: New liquidation mail title

--- src/mrsrequest/tests/test_mrsrequest_models.py
+++ src/mrsrequest/tests/test_mrsrequest_models.py
@@ -6,6 +6,7 @@ import uuid
 from django.utils import timezone
 from freezegun import freeze_time
 
+from person.models import Person
 from mrsrequest.models import MRSRequest
 
 
@@ -61,22 +62,86 @@ def test_mrsrequest_str():
     assert str(MRSRequest(display_id=300012301111)) == '300012301111'
 
 
-@pytest.mark.django_db
-def test_mrsrequest_increments_at_minute_zero():
-    paris = pytz.timezone('Europe/Paris')
-    paris_yesterday = datetime.datetime(1999, 12, 31, 0, 5, tzinfo=paris)
-    assert MRSRequest.objects.create(
-        creation_datetime=paris_yesterday).display_id == '199912310000'
+paris = pytest.fixture(lambda: pytz.timezone('Europe/Paris'))
 
+
+@pytest.fixture
+def paris_yesterday(paris):
+    return datetime.datetime(1999, 12, 31, 0, 5, tzinfo=paris)
+
+
+@pytest.fixture
+def utc_today():
     # this is 2000-01-01 in Europe/Paris
-    utc_today = datetime.datetime(1999, 12, 31, 23, 5, tzinfo=pytz.utc)
+    return datetime.datetime(1999, 12, 31, 23, 5, tzinfo=pytz.utc)
+
+
+@pytest.fixture
+def paris_today(paris):
+    # objects created with paris_today should be affected by those
+    # created with utc_today
+    return datetime.datetime(2000, 1, 1, 0, 5, tzinfo=paris)
+
+
+@pytest.mark.django_db
+def test_mrsrequest_increments_at_minute_zero(
+        paris, paris_yesterday, paris_today, utc_today):
+
+    assert MRSRequest.objects.create(
+        creation_datetime=paris_yesterday
+    ).display_id == '199912310000'
 
     # do not count the abouve as first
     assert MRSRequest.objects.create(
-        creation_datetime=utc_today).display_id == '200001010000'
-
-    paris_today = datetime.datetime(2000, 1, 1, 0, 5, tzinfo=paris)
+        creation_datetime=utc_today
+    ).display_id == '200001010000'
 
     # do not count the abouve as first
     assert MRSRequest.objects.create(
-        creation_datetime=paris_today).display_id == '200001010001'
+        creation_datetime=paris_today
+    ).display_id == '200001010001'
+
+
+def test_mrsrequest_day_number_three_digits(paris_yesterday):
+    assert MRSRequest(creation_datetime=paris_yesterday).day_number == '365'
+
+
+def test_mrsrequest_day_number_one_digit(paris_today):
+    assert MRSRequest(creation_datetime=paris_today).day_number == '001'
+
+
+def test_mrsrequest_day_number_timezone_conversion(utc_today):
+    assert MRSRequest(creation_datetime=utc_today).day_number == '001'
+
+
+@pytest.mark.django_db
+def test_mrsrequest_order_number(paris_yesterday, paris_today, utc_today):
+    person = Person.objects.create(
+        first_name='test', last_name='test', nir=1234567890123)
+
+    obj = MRSRequest.objects.create(
+        creation_datetime=utc_today, insured=person)
+    assert obj.order_number == '01'
+
+    obj = MRSRequest.objects.create(
+        creation_datetime=paris_yesterday, insured=person)
+    assert obj.order_number == '01'
+
+    # utc_today had impact on number, but not paris_yesterday
+    obj = MRSRequest.objects.create(
+        creation_datetime=paris_today, insured=person)
+    assert obj.order_number == '02'
+
+    for i in range(1, 100):
+        # start on 1 to have a different datetime from above
+        # not using bulk_create because of the signal
+        obj = MRSRequest.objects.create(
+            creation_datetime=paris_today + datetime.timedelta(seconds=i),
+            insured=person,
+        )
+        if i >= 98:
+            assert obj.order_number == '99'
+        else:
+            assert obj.order_number == '{:02d}'.format(i + 2), 'object #' + i
+
+    assert obj.order_number == '99'
