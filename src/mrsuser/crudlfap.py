15c69507b0bb45f9b4cd5ce9a44d1107eb6bf837
fjg <github@efji.net>
2018-04-26 14:53:30 +0200

    Ajout du champ is_active pour filtrer

--- src/mrsuser/crudlfap.py
+++ src/mrsuser/crudlfap.py
@@ -5,5 +5,6 @@ from .models import User
 crudlfap.site[User]['update'].fields.append('caisses')
 crudlfap.site[User]['create'].fields.append('caisses')
 crudlfap.site[User]['list'].filter_fields.append('caisses')
+crudlfap.site[User]['list'].filter_fields.append('is_active')
 crudlfap.site[User]['list'].table_fields.insert(0, 'caisses')
 crudlfap.site[User]['list'].table_fields.insert(1, 'is_active')
