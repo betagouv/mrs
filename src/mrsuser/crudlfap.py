5326501c0a3a18c877844e5ed70543511048a4bf
jpic <jamespic@gmail.com>
2018-04-29 18:00:13 +0200

    Fix #320: Remove Group crud

--- src/mrsuser/crudlfap.py
+++ src/mrsuser/crudlfap.py
@@ -1,6 +1,9 @@
 from crudlfap import crudlfap
 
+from django.contrib.auth.models import Group
+
 from .models import User
+
 
 crudlfap.site[User]['update'].fields.append('caisses')
 crudlfap.site[User]['create'].fields.append('caisses')
@@ -8,3 +11,4 @@ crudlfap.site[User]['list'].filter_fields.append('caisses')
 crudlfap.site[User]['list'].filter_fields.append('is_active')
 crudlfap.site[User]['list'].table_fields.insert(0, 'caisses')
 crudlfap.site[User]['list'].table_fields.insert(1, 'is_active')
+del crudlfap.site[Group]
