5c03309446537cde13ce548bcb02328e43569043
angelo510 <devwiz.sh@gmail.com>
2018-04-30 01:36:13 -0700

    remove duplicated code and import friendly

--- src/mrs/static/js/crudlfap.js
+++ src/mrs/static/js/crudlfap.js
@@ -2,31 +2,12 @@
 
 import { Application } from 'stimulus'
 import { definitionsFromContext } from 'stimulus/webpack-helpers'
-import M from 'materialize-css'
-import 'crudlfap/js/style.sass'
+import Crudlfap from 'crudlfap/js'
 
 (() => {
   if (window.Turbolinks === undefined) {
     var Turbolinks = require('turbolinks')
     Turbolinks.start()
-  }
-
-  //support to IE
-  if (!Element.prototype.matches) {
-    Element.prototype.matches = Element.prototype.msMatchesSelector ||
-      Element.prototype.webkitMatchesSelector
-  }
-
-  if (!Element.prototype.closest) {
-    Element.prototype.closest = function(s) {
-      var el = this
-      if (!document.documentElement.contains(el)) return null
-      do {
-        if (el.matches(s)) return el
-        el = el.parentElement || el.parentNode
-      } while (el != null && el.nodeType === 1)
-      return el
-    }
   }
 }).bind(window)()
 
@@ -34,19 +15,4 @@ const application = Application.start()
 var context = require.context('./controllers', true, /\.js$/)
 application.load(definitionsFromContext(context))
 
-context = require.context('crudlfap/js/controllers', true, /\.js$/)
-application.load(definitionsFromContext(context))
-
-// Manual controller teardown
-// https://github.com/stimulusjs/stimulus/issues/104
-document.addEventListener('turbolinks:before-render', function() {
-  application.controllers.forEach(function(controller) {
-    if(typeof controller.teardown === 'function') {
-      controller.teardown()
-    }
-  })
-})
-
-document.addEventListener('turbolinks:load', function(e) {
-  M.AutoInit(e.target.body)
-})
+Crudlfap()
