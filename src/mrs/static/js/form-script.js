a902f7751b18fd2f832f63a65a43abbea7de0d02
angelo510 <devwiz.sh@gmail.com>
2018-05-02 08:15:39 -0700

    check if valid

--- src/mrs/static/js/form-script.js
+++ src/mrs/static/js/form-script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
   $('select').change(function () {
     var str = $(this).parent().find('input').val()
     var result = str
-    if (str.indexOf(',') != -1) {
+    if (str && str.indexOf(',') != -1) {
       var strarr = str.split(',')
       result = strarr[strarr.length - 1]
     }
