ee00d876532fd62049d8caa549cb1e4e9b3199bb
jpic <jamespic@gmail.com>
2018-06-05 17:31:01 +0200

    Fix #390: User status

--- src/mrsuser/crudlfap.py
+++ src/mrsuser/crudlfap.py
@@ -3,7 +3,29 @@ from crudlfap import crudlfap
 from django import forms
 from django.contrib.auth.models import Group
 
+from django_filters import filters
+import django_tables2 as tables
+
 from .models import User
+
+
+class StatusFilter(filters.ChoiceFilter):
+    def __init__(self, *a, **k):
+        k.setdefault('choices', (
+            ('disabled', 'Désactivé'),
+            ('upn', 'UPN'),
+            ('admin', 'Admin'),
+        ))
+        k.setdefault('label', 'Statut')
+        super().__init__(*a, **k)
+
+    def filter(self, qs, value):
+        if value == 'disabled':
+            return qs.filter(is_active=False)
+        elif value == 'upn':
+            return qs.filter(is_superuser=False, is_staff=True)
+        elif value == 'admin':
+            return qs.filter(is_superuser=True)
 
 
 class UserForm(forms.ModelForm):
@@ -27,8 +49,13 @@ class UserForm(forms.ModelForm):
 del crudlfap.site[User].views['delete']
 crudlfap.site[User]['update'].fields.append('caisses')
 crudlfap.site[User]['create'].form_class = UserForm
-crudlfap.site[User]['list'].filter_fields.append('caisses')
-crudlfap.site[User]['list'].filter_fields.append('is_active')
+crudlfap.site[User]['list'].filter_fields = [
+    'status',
+    'caisses',
+]
+crudlfap.site[User]['list'].filterset_extra_class_attributes = dict(
+    status=StatusFilter()
+)
 crudlfap.site[User]['list'].table_fields = [
     'caisses',
     'is_active',
@@ -37,4 +64,7 @@ crudlfap.site[User]['list'].table_fields = [
     'last_name',
     'status',
 ]
+crudlfap.site[User]['list'].table_columns = dict(
+    status=tables.Column(verbose_name='statut'),
+)
 del crudlfap.site[Group]
