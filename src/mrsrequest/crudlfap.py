5264835968cc9d75bae7b51e9bc54d7e6cb047ba
jpic <jamespic@gmail.com>
2018-06-06 21:27:27 +0200

    First iteration for #252

--- src/mrsrequest/crudlfap.py
+++ src/mrsrequest/crudlfap.py
@@ -1,6 +1,16 @@
+import csv
+from datetime import datetime
+import io
+
 from crudlfap import crudlfap
 
+from django import forms
+from django import http
+from django.core.exceptions import ValidationError
+
 import django_tables2 as tables
+
+from institution.models import Institution
 
 from .views import (
     MRSRequestRejectView,
@@ -88,10 +98,147 @@ class MRSRequestListView(crudlfap.ListView):
         return self.queryset
 
 
+class MRSRequestExport(crudlfap.ObjectsView):
+    material_icon = 'cloud_download'
+    turbolinks = False
+
+    def get_objects(self):
+        self.objects = self.queryset.filter(mandate_date=None)
+        return self.objects
+
+    def get(self, request, *args, **kwargs):
+        f = io.StringIO()
+        w = csv.writer(f)
+        w.writerow((
+            'caisse',
+            'id',
+            'nir',
+            'naissance',
+            'nom',
+            'transport',
+            'mandatement',
+            'base',
+            'montant',
+            'bascule',
+            'finess',
+            'adeli',
+        ))
+        for obj in self.objects:
+            date_depart = obj.transport_set.order_by(
+                'date_depart'
+            ).first().date_depart
+
+            w.writerow((
+                str(obj.caisse),
+                obj.display_id,
+                obj.insured.nir,
+                obj.insured.birth_date.strftime('%d/%m/%Y'),
+                '{o.first_name} {o.last_name}'.format(o=obj.insured),
+                date_depart.strftime('%d/%m/%Y'),
+                '',
+                '',
+                '',
+                '',
+                '',
+                '',
+            ))
+
+        f.seek(0)
+        response = http.HttpResponse(f.read(), content_type='text/csv')
+        response['Content-Disposition'] = (
+            'attachment; filename="mrs-export-{}.csv"'.format(
+                self.objects.order_by('-creation_datetime').first().display_id
+            )
+        )
+        return response
+
+
+class MRSRequestImport(crudlfap.FormMixin, crudlfap.ModelView):
+    material_icon = 'cloud_upload'
+    turbolinks = False
+    form_invalid_message = 'Erreurs durant l\'import'
+    form_valid_message = 'Importé avec succès'
+
+    class form_class(forms.Form):
+        csv = forms.FileField()
+
+    def form_valid(self):
+        def decode_utf8(input_iterator):
+            for l in input_iterator:
+                yield l.decode('utf-8')
+
+        self.errors = dict()
+        self.success = dict()
+
+        f = csv.DictReader(decode_utf8(self.request.FILES['csv']))
+
+        for i, row in enumerate(f):
+            self.import_row(i, row)
+
+        self.keys = row.keys()
+        return self.render_to_response()
+
+    def import_row(self, i, row):
+        obj = self.queryset.filter(display_id=row['id']).first()
+
+        if obj:
+            self.import_obj(self, i, row, obj)
+        else:
+            self.errors[i] = dict(
+                row=row,
+                message='Demande introuvable en base de données'
+            )
+
+    def import_obj(self, i, row, obj):
+        if row['mandatement']:
+            obj.mandate_date = datetime.strptime(
+                row['mandatement'],
+                '%d/%m/%Y'
+            ).date()
+
+        obj.payment_base = row['base']
+        obj.payment_amount = row['montant']
+        obj.insured_shift = bool(row['bascule'])
+
+        if row['finess']:
+            obj.institution = self.institution_get_or_create(i, row)
+
+        self.save_obj(i, row, obj)
+
+    def save_obj(self, i, row, obj):
+        try:
+            obj.full_clean()
+        except ValidationError as e:
+            self.errors[i] = str(e)
+            return
+
+        try:
+            obj.save()
+        except Exception as e:
+            self.errors[i] = dict(row=row, message=e.message)
+        else:
+            self.success[i] = dict(object=obj, row=row)
+
+    def institution_get_or_create(self, i, row):
+        try:
+            Institution(finess=row['finess']).clean_fields()
+        except ValidationError:
+            self.errors[i] = dict(
+                row=row,
+                message='FINESS invalide {}'.format(row['finess'])
+            )
+        else:
+            return Institution.objects.get_or_create(
+                finess=row['finess']
+            )[0]
+
+
 class MRSRequestRouter(crudlfap.Router):
     model = MRSRequest
     material_icon = 'insert_drive_file'
     views = [
+        MRSRequestExport,
+        MRSRequestImport,
         MRSRequestValidateObjectsView,
         crudlfap.DeleteView,
         crudlfap.DetailView.clone(locks=True, title_heading=None),
@@ -110,7 +257,7 @@ class MRSRequestRouter(crudlfap.Router):
         if view.urlname in ('list', 'validateobjects'):
             return True  # secure by get_objects_for_user below
 
-        if view.urlname == 'delete':
+        if view.urlname in ('delete', 'export', 'import'):
             return user.is_superuser
 
         if getattr(view, 'object', None):
