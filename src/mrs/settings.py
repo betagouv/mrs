6858ec029e2729b09da3a5e48bf7f504588bd555
jpic <jamespic@gmail.com>
2018-08-23 20:16:25 +0200

    Serve staticfiles from uWSGI

--- src/mrs/settings.py
+++ src/mrs/settings.py
@@ -158,7 +158,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.getenv('STATIC_URL', '/static/')
+if not STATIC_URL.endswith('/'):
+    STATIC_URL += '/'
 STATIC_ROOT = os.getenv('STATIC_ROOT', os.path.join(BASE_DIR, 'collected'))
 STATICFILES_DIRS = [os.path.join(os.path.dirname(__file__), 'static')]
 
