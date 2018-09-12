6d90976792514257801a7fe8938fe65b66bb0cdc
jpic <jamespic@gmail.com>
2018-09-12 18:29:28 +0200

    Refactor MRSRequest forms
    One class name (MRSRequestForm) was defined twice, another was not used
    anymore but kept in case (MRSRequestAdminForm), and the first
    MRSRequestForm had part of its feature in the MRSRequestCreateForm so
    both were merged.
    
    Old test for MRSRequestAdminForm rewritten to nicely cover the
    attachment part from validation to relation save in obscure django
    conditions (with commit=False).
    
    Proactive tech debt payment in prevision for a new, and fully custom,
    updateform for some of the staff, pretty much the warmup needed for
    the next commit that has manual cooking which requires
    form.save(commit=False) ;)

--- src/mrsrequest/tests/test_mrsrequest_form.py
+++ src/mrsrequest/tests/test_mrsrequest_form.py
@@ -5,7 +5,7 @@ from freezegun import freeze_time
 import pytest
 
 from mrsattachment.models import MRSAttachment
-from mrsrequest.forms import MRSRequestAdminForm
+from mrsrequest.forms import MRSRequestCreateForm
 from mrsrequest.models import Bill, MRSRequest, PMT, Transport
 from person.models import Person
 
@@ -24,6 +24,18 @@ def person():
 @freeze_time('2017-12-19 05:51:11')
 @pytest.mark.dbdiff(models=[MRSAttachment, PMT, Person, Bill, Transport])
 def test_form_save_m2m(monkeypatch, person, caisse):
+    def _form(**extra):
+        data = dict()
+        data['caisse'] = [caisse.pk]
+        data['distance'] = ['100']
+        for k, v in extra.items():
+            data[k] = [str(v)]
+
+        form = MRSRequestCreateForm(data, mrsrequest_uuid=mrsrequest_uuid)
+
+        form.full_clean()
+        return form
+
     mrsrequest_uuid = '6bf490e6-4521-458a-adfe-8d4ef5a64687'
 
     monkeypatch.setattr(
@@ -31,37 +43,44 @@ def test_form_save_m2m(monkeypatch, person, caisse):
         lambda upload: upload.read()
     )
 
+    # PMT is only missing attachement at 0 expense
+    form = _form(expense=0)
+    assert not form.non_field_errors()
+    assert not form.is_valid()
+    assert list(form.errors.keys()) == ['pmt']
+
+    # Bills become required with expense
+    form = _form(expense=10)
+    assert not form.non_field_errors()
+    assert not form.is_valid()
+    assert list(form.errors.keys()) == ['pmt', 'bills']
+
     with io.BytesIO(b'test_mrsattachmentform0') as f:
         f.name = 'test_mrsattachmentform0.jpg'
         f.content_type = 'image/jpg'
         PMT.objects.record_upload(mrsrequest_uuid, f)
+
+    # Only Bills is missing now
+    form = _form(expense=10)
+    assert not form.non_field_errors()
+    assert not form.is_valid()
+    assert list(form.errors.keys()) == ['bills']
 
     with io.BytesIO(b'test_mrsattachmentform1') as f:
         f.name = 'test_mrsattachmentform1.jpg'
         f.content_type = 'image/jpg'
         Bill.objects.record_upload(mrsrequest_uuid, f)
 
-    data = dict(mrsrequest_uuid=[mrsrequest_uuid])
-    data['caisse'] = [caisse.pk]
-    data['date_depart'] = ['2017-02-02']
-    data['date_return'] = ['2017-02-02']
-    data['distance'] = ['100']
-    data['expense'] = ['10']
-    data['insured'] = [str(person.pk)]
-
-    # If this test method goes wrong about the uuid, make sure its not
-    # lost during ctor
-    form = MRSRequestAdminForm(data)
-
-    # Emulate what djang admin loves to do
-    form.full_clean()
+    # Is the form's save_m2m method going to relate the above uploads by
+    # uuid ?
+    form = _form(expense=10)
     assert not form.non_field_errors()
     assert not form.errors
     assert form.is_valid()
 
-    obj = form.save(commit=False)
-    obj.save()
-    form.save_m2m()
+    obj = form.save(commit=False)  # let's try that false commit,
+    obj.save()                     # with manual save,
+    form.save_m2m()                # and manual relation save.
 
     Fixture(
         './src/mrsrequest/tests/test_mrsrequest_form.json',  # noqa
