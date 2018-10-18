6fc530b834fad85011a9eea8daef7e69150d7ebf
vindarel <ehvince@mailz.org>
2018-10-18 12:12:05 +0200

    update savings on Person save signal
    for #650

--- src/mrsstat/tests/test_mrsstat.py
+++ src/mrsstat/tests/test_mrsstat.py
@@ -1,9 +1,11 @@
+import pytest
 from dbdiff.fixture import Fixture
 from django import test
 from freezegun import freeze_time
 
+from person.models import Person
 from mrsrequest.models import MRSRequest
-from mrsstat.models import update_stat_for_mrsrequest, Stat
+from mrsstat.models import update_stat_for_mrsrequest, stat_update_person, Stat
 
 
 class StatTest(test.TransactionTestCase):
@@ -22,3 +24,15 @@ class StatTest(test.TransactionTestCase):
             'mrsstat/tests/test_mrsstat.json',
             models=[Stat]
         ).assertNoDiff()
+
+
+@pytest.mark.django_db
+def test_stat_update_person_shifted(mock):
+    Fixture('mrs/tests/data.json').load()
+    stat_update_mock = mock.patch('mrsstat.models.stat_update')
+    pers = Person.objects.exclude(shifted=True).first()
+    pers.shifted = True
+    stat_update_person(Person, instance=pers)
+
+    for req in pers.mrsrequest_set.all():
+        stat_update_mock.assert_called_once_with(type(req), req)
