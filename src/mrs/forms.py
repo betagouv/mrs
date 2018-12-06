6d45e0a77f987aead3e77c6638320b3df7c0f56e
jpic <jpic@yourlabs.org>
2018-12-06 12:38:26 +0100

    Fix side effect from #726: non-native help text landed on native widget

--- src/mrs/forms.py
+++ src/mrs/forms.py
@@ -16,17 +16,18 @@ class DateInputNative(DateInput):
 
 
 class DateField(forms.DateField):
+    DEFAULT_HELP = 'Au format jj/mm/aaaa, exemple: 31/12/2000'
+
     def __init__(self, *args, **kwargs):
         # support both en and fr locales
         kwargs.setdefault('input_formats', ['%d/%m/%Y', '%Y-%m-%d'])
-        kwargs.setdefault(
-            'help_text',
-            'Au format jj/mm/aaaa, exemple: 31/12/2000'
-        )
+        kwargs.setdefault('help_text', self.DEFAULT_HELP)
         super().__init__(*args, **kwargs)
 
 
 class DateFieldNative(DateField):
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('widget', DateInputNative)
+        # prevent default help_text from datefield
+        kwargs.setdefault('help_text', None)
         super().__init__(*args, **kwargs)
