540c96ce582610763def4940a03185090c0cfa75
jpic <jpic@yourlabs.org>
2018-12-18 12:18:11 +0100

    Bugfix: make Caisse confirms count orderable

--- src/caisse/crudlfap.py
+++ src/caisse/crudlfap.py
@@ -1,9 +1,45 @@
 from crudlfap import shortcuts as crudlfap
 
+from django.db import models
+
 import django_tables2 as tables
 
 from .models import Caisse, Email
 
+
+class CaisseListView(crudlfap.ListView):
+    table_sequence = (
+        'code',
+        'name',
+        'number',
+        'active',
+        'score',
+        'confirms',
+    )
+
+    table_columns = dict(
+        confirms=tables.Column(
+            accessor='mrsrequest__confirms__sum',
+            verbose_name='Alertes',
+        )
+    )
+
+    search_fields = (
+        'code',
+        'name',
+        'number',
+    )
+
+    filter_fields = (
+        'active',
+    )
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.annotate(
+            models.Sum('mrsrequest__confirms')
+        )
+        return qs
 
 crudlfap.Router(
     Caisse,
@@ -14,30 +50,7 @@ crudlfap.Router(
         crudlfap.DeleteView,
         crudlfap.UpdateView,
         crudlfap.DetailView,
-        crudlfap.ListView.clone(
-            table_sequence=(
-                'code',
-                'name',
-                'number',
-                'active',
-                'score',
-                'confirms',
-            ),
-            table_columns=dict(
-                confirms=tables.Column(
-                    accessor='confirms',
-                    verbose_name='Alertes',
-                ),
-            ),
-            search_fields=(
-                'code',
-                'name',
-                'number',
-            ),
-            filter_fields=(
-                'active',
-            )
-        ),
+        CaisseListView,
     ]
 ).register()
 
