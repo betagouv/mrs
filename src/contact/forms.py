48d8184475e40bea302af4a4914308778521498d
vindarel <ehvince@mailz.org>
2018-12-19 13:14:02 +0100

    contact form: add motif and caisse
    for #804

--- src/contact/forms.py
+++ src/contact/forms.py
@@ -2,8 +2,22 @@ import material
 
 from django import forms
 
+from caisse.forms import ActiveCaisseChoiceField
 
 class ContactForm(forms.Form):
+    motif = forms.ChoiceField(
+        label='Motif',
+        choices=(
+            (None, '---------'),
+            ('request_error', 'Erreur dans votre demande'),
+            ('request_question', 'Réclamation sur votre demande'),
+            ('website_question', 'Question sur le site'),
+            ('other', 'Autre'),
+        ),
+    )
+    caisse = ActiveCaisseChoiceField(
+        label='Votre caisse de rattachement',
+    )
     nom = forms.CharField()
     email = forms.EmailField()
     message = forms.CharField(widget=forms.Textarea)
@@ -11,6 +25,8 @@ class ContactForm(forms.Form):
     layout = material.Layout(
         material.Fieldset(
             ' ',
+            'motif',
+            'caisse',
             material.Row(
                 'nom',
                 'email',
