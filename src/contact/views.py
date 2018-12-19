d72d58c192618866ca1cb36d4a8123be23d709b4
vindarel <ehvince@mailz.org>
2018-12-19 13:38:05 +0100

    contact form: add "motif" and "caisse.name" in message body
    for #804

--- src/contact/views.py
+++ src/contact/views.py
@@ -5,6 +5,7 @@ from django.views import generic
 from djcall.models import Caller
 
 from .forms import ContactForm
+from .forms import get_motif
 
 
 class ContactView(generic.FormView):
@@ -24,13 +25,18 @@ class ContactView(generic.FormView):
                 'contact/team_mail_title.txt'
             ).render(dict(form=form)).strip()
 
+        body=template.loader.get_template(
+            'contact/team_mail_body.txt'
+        ).render(dict(
+            form=form,
+            motif=get_motif(form.cleaned_data.get('motif')),
+        )).strip()
+
         Caller(
             callback='djcall.django.email_send',
             kwargs=dict(
                 subject=subject,
-                body=template.loader.get_template(
-                    'contact/team_mail_body.txt'
-                ).render(dict(form=form)).strip(),
+                body=body,
                 to=[settings.TEAM_EMAIL],
                 reply_to=[form.cleaned_data['email']],
             ),
