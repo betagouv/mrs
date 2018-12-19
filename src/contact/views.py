369673ef25f48dadb12334894811e95b460bed72
vindarel <ehvince@mailz.org>
2018-12-19 13:48:19 +0100

    contact form: send mail to the caisse when necessary
    for #804

--- src/contact/views.py
+++ src/contact/views.py
@@ -8,6 +8,12 @@ from .forms import ContactForm
 from .forms import get_motif
 
 
+def message_for_caisse(form):
+    if form.cleaned_data.get('motif').startswith('request'):
+        return True
+    return False
+
+
 class ContactView(generic.FormView):
     template_name = 'contact/form.html'
     form_class = ContactForm
@@ -16,7 +22,7 @@ class ContactView(generic.FormView):
         self.success = True
 
         subject = ""
-        if form.cleaned_data.get('motif').startswith('request'):
+        if message_for_caisse(form):
             subject = template.loader.get_template(
                 'contact/team_mail_reclamation_mrs.txt'
             ).render().strip()
@@ -25,19 +31,24 @@ class ContactView(generic.FormView):
                 'contact/team_mail_title.txt'
             ).render(dict(form=form)).strip()
 
-        body=template.loader.get_template(
+        body = template.loader.get_template(
             'contact/team_mail_body.txt'
         ).render(dict(
             form=form,
             motif=get_motif(form.cleaned_data.get('motif')),
         )).strip()
 
+        to = [settings.TEAM_EMAIL]
+        if message_for_caisse(form):
+            caisse = form.cleaned_data.get('caisse')
+            to = [caisse.liquidation_email]
+
         Caller(
             callback='djcall.django.email_send',
             kwargs=dict(
                 subject=subject,
                 body=body,
-                to=[settings.TEAM_EMAIL],
+                to=to,
                 reply_to=[form.cleaned_data['email']],
             ),
         ).spool('mail')
