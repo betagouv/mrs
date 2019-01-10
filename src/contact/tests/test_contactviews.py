0df82deca171b5c37025680bcb977f30a7926519
jpic <jpic@yourlabs.org>
2019-01-10 01:16:52 +0100

    Contact: encapsulation and tests

--- src/contact/tests/test_contactviews.py
+++ src/contact/tests/test_contactviews.py
@@ -1,23 +1,46 @@
 import pytest
 
-from caisse.models import Caisse
+from django.conf import settings
+
 from contact.forms import ContactForm
-from contact.views import get_email_data
 
 
-@pytest.mark.django_db
-def test_contactview(p, srf, caisse):
-    form_data = dict(
+@pytest.fixture
+def data(caisse):
+    return dict(
         motif='request_error',
         caisse=caisse.pk,
         nom='alice',
         email='example@example.com',
-        message="J'écris \"selfalut l'monde.\".",
+        message='J\'écris "selfalut l\'monde.".',
     )
 
-    form = ContactForm(form_data)
+
+@pytest.mark.django_db
+def test_contactform_email_kwargs_request(p, srf, caisse, data):
+    form = ContactForm(data)
     assert form.is_valid()
-    email_data = get_email_data(form)
-    assert email_data['subject'] == 'RÉCLAMATION MRS'
-    assert caisse.liquidation_email in email_data['to']
-    assert 'Motif: J\'ai fait une erreur' in email_data['body']
+    kwargs = form.get_email_kwargs()
+    assert kwargs['subject'] == 'RÉCLAMATION MRS'
+    assert caisse.liquidation_email in kwargs['to']
+    assert 'Motif: J\'ai fait une erreur' in kwargs['body']
+
+
+@pytest.mark.django_db
+def test_contactform_email_kwargs_request_other(p, srf, caisse, data):
+    # fixes regression #851
+    data['caisse'] = 'other'
+    form = ContactForm(data)
+    assert form.is_valid()
+    kwargs = form.get_email_kwargs()
+    assert settings.TEAM_EMAIL in kwargs['to']
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('motif', ['website_question', 'other'])
+def test_contactform_email_kwargs_suggestion(p, srf, caisse, data, motif):
+    data['motif'] = motif
+    form = ContactForm(data)
+    assert form.is_valid()
+    kwargs = form.get_email_kwargs()
+    assert settings.TEAM_EMAIL in kwargs['to']
--- src/contact/views.py
+++ src/contact/views.py
@@ -1,5 +1,3 @@
-from django import template
-from django.conf import settings
 from django.views import generic
 
 from djcall.models import Caller
@@ -7,41 +5,14 @@ from djcall.models import Caller
 from .forms import ContactForm
 
 
-def get_email_data(form):
-    to = [settings.TEAM_EMAIL]
-
-    if form.cleaned_data.get('motif').startswith('request'):
-        subject = 'RÉCLAMATION MRS'
-        email = getattr(form.cleaned_data['caisse'], 'liquidation_email', None)
-
-        if email:  # in case caisse == 'other', let TEAM_EMAIL
-            to = [email]
-    else:
-        subject = 'Nouveau message envoyé depuis le site'
-
-    body = template.loader.get_template(
-        'contact/contact_mail_body.txt'
-    ).render(dict(
-        form=form,
-        motif=dict(form.fields['motif'].choices)[form.cleaned_data['motif']],
-    )).strip()
-
-    return dict(subject=subject, body=body, to=to)
-
-
 class ContactView(generic.FormView):
     template_name = 'contact/form.html'
     form_class = ContactForm
 
     def form_valid(self, form):
-        self.success = True
-
-        email_data = get_email_data(form)
-        email_data['reply_to'] = [form.cleaned_data['email']]
-
         Caller(
             callback='djcall.django.email_send',
-            kwargs=email_data,
+            kwargs=form.get_email_kwargs(),
         ).spool('mail')
-
+        self.success = True
         return generic.TemplateView.get(self, self.request)
