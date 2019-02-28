4d38469d6395ae9814da60abecf8c0e4e97ec525
jpic <jpic@yourlabs.org>
2019-02-28 02:17:31 +0100

    Implement Rating Router

--- src/rating/crudlfap.py
+++ src/rating/crudlfap.py
@@ -3,4 +3,13 @@ from crudlfap import shortcuts as crudlfap
 from .models import Rating
 
 
-crudlfap.Router(Rating).register()
+class RatingRouter(crudlfap.Router):
+    model = Rating
+    allowed_groups = ['Admin']
+    material_icon = 'star'
+    views = [
+        crudlfap.DetailView,
+        crudlfap.ListView,
+    ]
+
+RatingRouter().register()
