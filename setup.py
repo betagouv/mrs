e4f16aa79dd76c7d1e65f1012b60b775ffa544e0
jpic <jpic@yourlabs.org>
2019-05-10 10:35:19 +0200

    Add docker-compose.yml

--- setup.py
+++ setup.py
@@ -16,23 +16,10 @@ setup(
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
-    license = 'MIT',
     install_requires = install_reqs,
     entry_points = {
         'console_scripts': [
             'mrs = mrs.manage:main',
         ],
     },
-    classifiers = [
-        'Development Status :: 1 - Planning',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
 )
