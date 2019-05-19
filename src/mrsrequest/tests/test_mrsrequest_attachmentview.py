7dbc3894967049b7d90b388e1e6a3a2de3ed61a4
jpic <jpic@yourlabs.org>
2019-05-19 10:49:43 +0200

    Bugfix multigroup admin PMT download permission

---
+++ src/mrsrequest/tests/test_mrsrequest_attachmentview.py
@@ -0,0 +1,44 @@
+import pytest
+
+from dbdiff.fixture import Fixture
+from django.test import Client
+
+from mrsuser.models import User
+from mrsrequest.models import MRSRequest, PMT
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('groups', [
+    ['stat', 'support'],
+    ['superviseur'],
+    ['support'],
+    ['upn', 'support'],
+    ['upn'],
+    ['stat', 'upn'],
+])
+def test_mrsfiledownloadview_admins(groups, request_factory, caisse):
+    Fixture('./src/mrs/tests/data.json').load()
+
+    mrsrequest = MRSRequest(caisse=caisse)
+    mrsrequest.pmt = PMT.objects.create(
+        mrsrequest_uuid=mrsrequest.id,
+        filename='test_mrsrequestcreateview_story.jpg',
+        binary=b'test_mrsrequestcreateview_story',
+    )
+    mrsrequest.save()
+    mrsrequest.save_pmt()
+
+    user = User(username='test_mrsfileuploadview_admins')
+    user.set_password('secret')
+    user.save()
+    user.add_groups(groups)
+    user.caisses.add(caisse)
+
+    client = Client(user=user)
+    client.login(username='test_mrsfileuploadview_admins', password='secret')
+    response = client.get(mrsrequest.pmt.get_download_url())
+    assert response.status_code == 200
+
+    user.caisses.remove(caisse)
+    response = client.get(mrsrequest.pmt.get_download_url())
+    assert response.status_code == 404
