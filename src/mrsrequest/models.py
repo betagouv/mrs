02139f3f85371514dd7f2e4cf32ce20135ba4d0e
jpic <jpic@yourlabs.org>
2019-06-03 17:34:41 +0200

    Typo in distance help text

--- src/mrsrequest/models.py
+++ src/mrsrequest/models.py
@@ -359,7 +359,7 @@ class MRSRequest(models.Model):
         verbose_name='Distance (km)',
         help_text='Indiquez le nombre total de kilomètres parcourus:'
         ' nombre de dates multiplié par le nombre de kilomètres.'
-        ' Par exemple, vous réalisez 2 aller/retour a 10km de chez vous soit'
+        ' Par exemple, vous réalisez 2 aller/retour à 10km de chez vous soit'
         ' 4 dates multipliées par 10 kilomètres: déclarez 80 kilomètres'
         ' parcourus.',
         null=True,
