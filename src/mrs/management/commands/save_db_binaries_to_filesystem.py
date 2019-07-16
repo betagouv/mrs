627687af19ef59bc22bfdac555aee9f65c2d3bf5
Jean-Baptiste Mora <jb@moramora.fr>
2019-07-16 12:34:14 +0200

    WIP - Put attachments files out of DB (4)
    Update data migration script

--- src/mrs/management/commands/save_db_binaries_to_filesystem.py
+++ src/mrs/management/commands/save_db_binaries_to_filesystem.py
@@ -3,38 +3,49 @@ import io
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.core.management.base import BaseCommand
 
-from mrsrequest.models import MRSRequest
+from mrsrequest.models import PMT, Bill
 
 
 class Command(BaseCommand):
     help = 'Save DB file binaries to filesystem'
 
+    def save_binary_to_file(self, obj):
+        if not obj.attachment_file:
+            self.stdout.write(
+                self.style.ERROR('{} has no attachment_file')
+                    .format(obj)
+            )
+
+            try:
+                # Stream binary to in-memory Django file
+                f = InMemoryUploadedFile(
+                    io.BytesIO(obj.binary),
+                    'field_name',
+                    'test_file.png',
+                    'image/png',
+                    len(obj.binary),
+                    None,
+                )
+
+                # Save it to the model (which will save it to the filesystem)
+                obj.attachment_file = f
+                obj.save()
+
+                self.stdout.write(
+                    self.style.SUCCESS('   OK !')
+                )
+            except Exception as e:
+                self.stdout.write(
+                    self.style.ERROR('   Error : {}'.format(e))
+                )
+
     def handle(self, *args, **options):
-        for mrsrequest in MRSRequest.objects.all():
-            for pmt in mrsrequest.pmt_set.all():
-                if not pmt.attachment_file:
-                    self.stdout.write(
-                        self.style.ERROR('{} has no attachment_file')
-                            .format(pmt)
-                    )
+        # Scan all PMTs / Bills for ones with no 'attachment_file'
+        for pmt in PMT.objects.all():
+            self.save_binary_to_file(pmt)
+        for bill in Bill.objects.all():
+            self.save_binary_to_file(bill)
 
-                    try:
-                        f = InMemoryUploadedFile(
-                            io.BytesIO(pmt.binary),
-                            'field_name',
-                            'test_file.png',
-                            'image/png',
-                            len(pmt.binary),
-                            None,
-                        )
-
-                        pmt.attachment_file = f
-                        pmt.save()
-
-                        self.stdout.write(
-                            self.style.SUCCESS('   OK !')
-                        )
-                    except Exception as e:
-                        self.stdout.write(
-                            self.style.ERROR('   Error : {}'.format(e))
-                        )
+        self.stdout.write(
+            self.style.SUCCESS('--- END ---')
+        )
