635e1508d42edd85a9342fd7864e319ddfa0fba7
jpic <jpic@yourlabs.org>
2020-07-03 10:18:50 +0200

    Fix prune.sh script to not rm backups
    See restic faq about check errors caused by packs not referenced by any
    index

--- ansible/templates/prune.sh
+++ ansible/templates/prune.sh
@@ -3,8 +3,6 @@ pushd {{ home }}
 export RESTIC_PASSWORD_FILE=.restic_password
 export RESTIC_REPOSITORY={{ lookup('env', 'RESTIC_REPOSITORY') or home + '/restic' }}
 restic forget --keep-last 7 --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 75
-if ! restic check; then
-    rm -rf $RESTIC_REPOSITORY && mv .${RESTIC_REPOSITORY}.backup $RESTIC_REPOSITORY
-    exit 1
-fi
 restic prune
+restic rebuild-index
+restic check
